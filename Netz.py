c0fb3c7f316c5178056353811c6d87a683dfc1fb
JojoB96 <johbeier@hotmail.de>
2020-06-11 09:41:29 +0200

    first notes

--- Netz.py
+++ Netz.py
@@ -6,3 +6,22 @@ Created on Thu Jun 11 09:28:46 2020
 @author: johanna
 """
 
+##Game: Mancala
+
+#Regeln: 
+#
+#
+#
+#
+#
+#
+#
+
+#current state: vector of length 14 (sum od entries is 48 / 72)
+#               boolean: Netzistdran
+
+#def turn(i)
+#   for i<j<vector(i)+i:
+#       vector(j%14) = vector(j%14)+1
+#
+#  Regeln berücksichtigen
