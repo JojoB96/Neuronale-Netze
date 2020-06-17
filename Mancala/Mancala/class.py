06ebe597f6e337c68e21d03c8e5b4191ccfff0c7
JojoB96 <johbeier@hotmail.de>
2020-06-17 11:44:54 +0200

    simple rules

--- Mancala/Mancala/class.py
+++ Mancala/Mancala/class.py
@@ -13,15 +13,47 @@ class Mancala(object):
     
     
     #feld = Zahl zwischen 0 und 11
-    def spielzug(self, feld):
-        if(feld > 11):
+    def spielzug1(self, feld):
+        if(feld > 6):
+            raise ValueError("feld ungueltig")
+        
+        l = self.spielfeld[feld]
+        self.spielfeld[feld] =0
+        
+        #spielen
+        for i in range(l):
+            self.spielfeld[(feld+i+1) % 12] =  self.spielfeld[(feld+i+1) %12]+1
+       
+        #schlagen
+        if (self.spielfeld[(feld+l) %12] == 2 or self.spielfeld[(feld+l) %12] == 4 or self.spielfeld[(feld+l) %12] == 6):
+            self.spielfeld[12] =self.spielfeld[12]+self.spielfeld[(feld+l) %12]
+            self.spielfeld[(feld+l) %12] = 0
+            print("treffer")
+            
+    def spielzug2(self, feld):
+        if(5>feld >12):
             raise ValueError("feld ungueltig")
         
         l = self.spielfeld[feld]
         self.spielfeld[feld] =0
         for i in range(l):
             self.spielfeld[(feld+i+1) % 12] =  self.spielfeld[(feld+i+1) %12]+1
-        
+       
+        #schlagen
+        if (self.spielfeld[(feld+l) %12] == 2 or self.spielfeld[(feld+l) %12] == 4 or self.spielfeld[(feld+l) %12] == 6):
+            self.spielfeld[13] =self.spielfeld[12]+self.spielfeld[(feld+l) %12]
+            self.spielfeld[(feld+l) %12] = 0
+            print("treffer")
+              
 ma = Mancala()
-ma.spielzug(11)       
-print(ma.spielfeld)             
+print(ma.spielfeld)
+ma.spielzug1(5)
+print(ma.spielfeld)   
+ma.spielzug2(8)       
+print(ma.spielfeld)  
+ma.spielzug1(4)
+print(ma.spielfeld)   
+ma.spielzug2(9)       
+print(ma.spielfeld)   
+ma.spielzug1(0)
+print(ma.spielfeld)                        
