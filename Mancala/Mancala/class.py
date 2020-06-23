553e7c437d34054e068293c48e265e87964cce10
v-kosin <viktor-kosin@gmx.de>
2020-06-23 20:25:56 +0200

    Class von Mancala erweitert um ein paar Funktionen für das Neuronale Netzwerk

--- Mancala/Mancala/class.py
+++ Mancala/Mancala/class.py
@@ -7,11 +7,28 @@ Created on Wed Jun 17 10:47:20 2020
 """
 import numpy as np
 import random
+import Network
 
 class Mancala(object):
-    def __init__(self):
-        self.spielfeld = np.array([6,6,6,6,6,6,6,6,6,6,6,6,0,0])  #Spielfeld (zustand)
+    def __init__(self, exploration_rate = None, network_layers = None , name = None):
+    # the Parameters are optional and are used to initialize a suitable neuronal network for the Game:
+    #   exploration_rate:   scalar, determines with wich rate an random action should be done
+    #   network_layers  :   scalar, contains the number of layers for the neuronal network
+    #   name            :   string, is used to call the data of bias and weights 
     
+        if exploration_rate is None or network_layers is None or name is None:
+            self.spielfeld          = np.array([6,6,6,6,6,6,6,6,6,6,6,6,0,0])  #Spielfeld (zustand)
+            print("Watch out! Due to missing parameters a neuronal network is not initialized yet!")
+        else:
+            self.spielfeld          = np.array([6,6,6,6,6,6,6,6,6,6,6,6,0,0])  #Spielfeld (zustand)
+            self.exploration_rate   = exploration_rate
+            self.rewards            = [1.0]
+    
+            self.net                = Network.Network(network_layers, name)
+        
+            # Parameter for the q-function
+            self.a                  = 0.3
+            self.discount           = 0.4
     
     #feld = Zahl zwischen 0 und 11
     def spielzug1(self, feld):
@@ -52,9 +69,40 @@ class Mancala(object):
     def randomspielzug1(self):
         r = random.randint(0,5)
         self.spielzug1(r)
+        
     def randomspielzug2(self):
         r = random.randint(6,11)
         self.spielzug2(r)
+        
+    def guess_Q(self, spielfeld):
+        # guess the q-value of the current spielfeld
+        return self.net.feedforward(spielfeld)
+    
+    def greedy_action(self, spielfeld):
+        #choose the action that will end to the highest q-value (according to the neuronal network)
+        return np.argmax(self.guess_Q(spielfeld))
+    
+    # def random_action(self, spielfeld)            ???
+    
+    def get_next_action(self, spielfeld):
+        if random.random() > self.exploration_rate:
+            return self.greedy_action(spielfeld)
+        else:
+            return #self.random_action()            Hier müssen wir uns nochmal genau überlegen, wo wir die regeln einbauen wollen
+        
+    def create_training_data(self, spielfeld_liste):
+        #
+        # ...
+        #
+        return [(s,q) for s,q in zip(spielfeld_liste, q_liste)]
+    
+    def train_net(self, iterations):
+        for i in range(iterations):
+            #spielfeld_liste =                      Hier benötigen wir dann noch geeignete Spielfunktionen
+            training_data = create_training_data(spielfeld_liste)
+            #self.net.                              Bisher hab ich noch keine Methode für die stochastische Gradientenmethode, kommt noch
+            
+        # evtl. speichern wir jedes mal / ab und zu die Gewichte und Bias (net.save_network.to_files(name))
               
 ma = Mancala()
 
