dee4c821429a1c1f072f8e4e5c9155ada39159ae
JojoB96 <johbeier@hotmail.de>
2020-06-27 16:56:44 +0200

    Spielfeldliste erzeugt

---
+++ Mancala/Mancala/Network.py
@@ -0,0 +1,91 @@
+# standard library
+import random
+
+# Basic math tools (Third-party library)
+import numpy as np
+
+def sigmoid(z):
+    x = np.exp( z ) 
+    return x / ( 1.0 + x )
+
+def d_sigmoid(z):
+    x = sigmoid( z )
+    return x*(1-x)
+
+def sigmoid_perceptron(activation, weight, bias):
+    return sigmoid( np.dot(weight, activation) + bias )
+
+class Network(object):
+    
+    def __init__(self, layers = None, name = None):
+        if layers is None:    
+            self.layers = 0
+            self.biases = []
+            self.weights = []
+        elif name is None:
+            self.layers = layers
+            self.biases = []
+            self.weights = []
+        else:
+            self.layers = layers
+            self.load_network_from_files(name)
+
+    def load_network_from_files(self, name):
+        self.biases = []
+        self.weights = []
+        
+        for i in range(0,self.layers):
+            i += 1
+            self.biases.append(np.loadtxt(name+"."+"bias."+str(i)+".csv", delimiter=",", ndmin=1))
+            self.weights.append(np.loadtxt(name+"."+"weights."+str(i)+".csv", delimiter=",", ndmin=2))
+            
+    def save_network_to_files(self, name):
+        for i in range(0,self.layers):
+            i += 1
+            np.savetxt(name+"."+"bias."+str(i)+".csv", self.biases[i-1], delimiter=",")
+            np.savetxt(name+"."+"weights."+str(i)+".csv", self.weights[i-1], delimiter=",")
+                
+    
+    def feedforward(self, activation):
+        for it in range(self.layers):
+            forward = np.dot(self.weights[it], activation) + self.biases[it]
+            activation = sigmoid(forward)
+        
+        return activation
+    
+    def feed_next(self, activation, layer):
+        z = np.dot(self.weights[layer], activation) + self.biases[layer]
+        n = sigmoid(z)
+        return n,z
+        
+    def backpropagation(self, activation, expected_result):
+        activations = [activation]
+        zs = []
+        for i in range(self.layers):
+            a, z = self.feed_next(activations[i], i)
+            activations.append(a)
+            zs.append(z)
+            
+        grad_b = np.array([np.zeros(b.shape) for b in self.biases])
+        grad_w = np.array([np.zeros(w.shape) for w in self.weights])
+        grad_b[-1] = (activations[self.layers] - expected_result) * d_sigmoid(zs[-1])
+        grad_w[-1] = np.outer(grad_b[-1],activations[-2])
+        for i in range(1,self.layers):
+            grad_b[-i-1] = np.dot(self.weights[-i].T, grad_b[-i])*d_sigmoid(zs[-i-1])
+            grad_w[-i-1] = np.outer(grad_b[-i-1],activations[-i-2])
+            
+        return grad_b, grad_w
+    
+    def update_weights_and_bias(self, mini_batch, eta):
+        grad_b = np.array([np.zeros(b.shape) for b in self.biases])
+        grad_w = np.array([np.zeros(w.shape) for w in self.weights])
+        
+        for approx, exact in mini_batch:
+            delta_b, delta_w = self.backpropagation(approx, exact)
+            grad_b = np.add(grad_b,delta_b)
+            grad_w = np.add(grad_w,delta_w)
+
+        for i in range(self.layers):
+            self.biases[i] -= eta/len(mini_batch)*grad_b[i]
+            self.weights[i] -= eta/len(mini_batch)*grad_w[i]
+
--- Mancala/Mancala/class.py
+++ Mancala/Mancala/class.py
@@ -8,6 +8,7 @@ Created on Wed Jun 17 10:47:20 2020
 import numpy as np
 import random
 import Network
+from copy import deepcopy
 
 class Mancala(object):
     def __init__(self, exploration_rate = None, network_layers = None , name = None):
@@ -16,10 +17,10 @@ class Mancala(object):
     #   network_layers  :   scalar, contains the number of layers for the neuronal network
     #   name            :   string, is used to call the data of bias and weights 
     
-        if exploration_rate is None or network_layers is None or name is None:
-            self.spielfeld          = np.array([6,6,6,6,6,6,6,6,6,6,6,6,0,0])  #Spielfeld (zustand)
-            print("Watch out! Due to missing parameters a neuronal network is not initialized yet!")
-        else:
+      ##  if exploration_rate is None or network_layers is None or name is None:
+        #    self.spielfeld          = np.array([6,6,6,6,6,6,6,6,6,6,6,6,0,0])  #Spielfeld (zustand)
+         #   print("Watch out! Due to missing parameters a neuronal network is not initialized yet!")
+        #else:
             self.spielfeld          = np.array([6,6,6,6,6,6,6,6,6,6,6,6,0,0])  #Spielfeld (zustand)
             self.exploration_rate   = exploration_rate
             self.rewards            = [1.0]
@@ -29,6 +30,7 @@ class Mancala(object):
             # Parameter for the q-function
             self.a                  = 0.3
             self.discount           = 0.4
+            self.spieler1           = True
     
     #feld = Zahl zwischen 0 und 11
     def spielzug1(self, feld):
@@ -39,7 +41,6 @@ class Mancala(object):
         
         l = self.spielfeld[feld]
         self.spielfeld[feld] =0
-        
         #spielen
         for i in range(l):
             self.spielfeld[(feld+i+1) % 12] =  self.spielfeld[(feld+i+1) %12]+1
@@ -49,7 +50,8 @@ class Mancala(object):
             self.spielfeld[12] =self.spielfeld[12]+self.spielfeld[(feld+l) %12]
             self.spielfeld[(feld+l) %12] = 0
             #print("treffer")
-            
+        return self.spielfeld
+        
     def spielzug2(self, feld):
         
         if(5>feld >12):
@@ -59,28 +61,32 @@ class Mancala(object):
         self.spielfeld[feld] =0
         for i in range(l):
             self.spielfeld[(feld+i+1) % 12] =  self.spielfeld[(feld+i+1) %12]+1
-       
+
         #schlagen
         if (self.spielfeld[(feld+l) %12] == 2 or self.spielfeld[(feld+l) %12] == 4 or self.spielfeld[(feld+l) %12] == 6):
             self.spielfeld[13] =self.spielfeld[13]+self.spielfeld[(feld+l) %12]
             self.spielfeld[(feld+l) %12] = 0
-            #print("treffer")
-            
-    def randomspielzug1(self):
-        r = random.randint(0,5)
-        self.spielzug1(r)
+        return(self.spielfeld)
         
-    def randomspielzug2(self):
-        r = random.randint(6,11)
-        self.spielzug2(r)
+    def randomfeld(self):
+        if self.spieler1 == True:
+            r = random.randint(0,5)
+            
+        else:
+            r = random.randint(6,11)
+        return r
         
     def guess_Q(self, spielfeld):
         # guess the q-value of the current spielfeld
         return self.net.feedforward(spielfeld)
     
     def greedy_action(self, spielfeld):
-        #choose the action that will end to the highest q-value (according to the neuronal network)
-        return np.argmax(self.guess_Q(spielfeld))
+        #choose the action that will end to the highest q-value (according to the neural network)
+        if self.spieler1 == True:
+            return np.argmax(self.guess_Q(spielfeld[0:6]))
+        else:
+            return np.argmax(self.guess_Q(spielfeld[7:12]))+7
+
     
     # def random_action(self, spielfeld)            ???
     
@@ -88,36 +94,42 @@ class Mancala(object):
         if random.random() > self.exploration_rate:
             return self.greedy_action(spielfeld)
         else:
-            return #self.random_action()            Hier müssen wir uns nochmal genau überlegen, wo wir die regeln einbauen wollen
+            return self.randomfeld()          #  Hier müssen wir uns nochmal genau überlegen, wo wir die regeln einbauen wollen
+     
+    def take_action(self, feld):
+        if self.spieler1 == True:
+            self.spieler1 = False
+            return(self.spielzug1(feld))
+        else:
+            self.spieler1 = True
+            return(self.spielzug2(feld))
+            
         
+    def play(self):
+        Spielfeldliste = [deepcopy(self.spielfeld)]
+        while not(np.array_equal(self.spielfeld[0:6] ,[0,0,0,0,0,0]) or np.array_equal(self.spielfeld[7:12] ,[0,0,0,0,0,0])):
+            feld = self.get_next_action(self.spielfeld)
+            self.spielfeld = self.take_action(feld)
+            Spielfeldliste.append(deepcopy(self.spielfeld))
+        return Spielfeldliste
+    
     def create_training_data(self, spielfeld_liste):
-        #
-        # ...
-        #
+        q_liste = []
+        for i in spielfeld_liste:
+            q = self.guess_Q(i)
+            q_liste.append(q)
+            
         return [(s,q) for s,q in zip(spielfeld_liste, q_liste)]
     
     def train_net(self, iterations):
         for i in range(iterations):
-            #spielfeld_liste =                      Hier benötigen wir dann noch geeignete Spielfunktionen
-            training_data = create_training_data(spielfeld_liste)
+            spielfeld_liste = self.play() #          Hier benötigen wir dann noch geeignete Spielfunktionen
+            training_data = self.create_training_data(spielfeld_liste)
             #self.net.                              Bisher hab ich noch keine Methode für die stochastische Gradientenmethode, kommt noch
-            
+        print(training_data)   
         # evtl. speichern wir jedes mal / ab und zu die Gewichte und Bias (net.save_network.to_files(name))
               
-ma = Mancala()
+ma = Mancala(exploration_rate = 0.4)
 
-for i in range(200):
-    if(np.array_equal(ma.spielfeld[0:6] ,[0,0,0,0,0,0])):
-        print("Spiel zu ende")
-        break
-    ma.randomspielzug1()
-    if(np.array_equal(ma.spielfeld[7:12] ,[0,0,0,0,0,0])):
-        print("Spiel zu ende")
-        break 
-    ma.randomspielzug2()
-  
-   # print(ma.spielfeld)    
-   
-print("Spielzuege:", 2*i)
-print(ma.spielfeld) 
-print(np.sum(ma.spielfeld))              
+       
+ma.train_net(5)
